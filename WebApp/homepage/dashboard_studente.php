48297ad5988350acdc1e55ad5056e31d2622e27d
Mattia Samanni <smatjoy06@icloud.com>
2025-05-06 09:08:17 +0200

    added show monete function on dashboard studente

--- WebApp/homepage/dashboard_studente.php
+++ WebApp/homepage/dashboard_studente.php
@@ -24,12 +24,16 @@ if ($role == "docente") {
 
 // mostrare le informazioni sull'utente attuale
 echo "<h2>Le tue informazioni:</h2>";
+
+echo "<b>Ruolo:</b> Studente<br>";
+
 echo "<b>Nome:</b> " . $nome . "<br>";
 
 echo "<b>Cognome:</b> " . $cognome . "<br>";
 
 echo "<b>Codice fiscale:</b> " . $codiceFiscale . "<br>";
 
+echo '<button type="button" onclick="window.location.href=\'../logout.php\'">Logout</button><br>';
 
 
 echo "<hr>";
@@ -47,16 +51,20 @@ echo "<hr>";
 // mostrare le classi in cui è lo studente
 $stmt = $mysqli->prepare("
 SELECT
-classevirtuale.Classe,
-classevirtuale.Materia,
-docente.Nome,
-docente.Cognome,
-classevirtuale.IdClasse
+    classevirtuale.Classe,
+    classevirtuale.Materia,
+    docente.Nome,
+    docente.Cognome,
+    classevirtuale.IdClasse,
+    IFNULL(SUM(partita.Monete), 0) AS MoneteTotali
 FROM studente
 JOIN iscrizione ON studente.CodiceFiscale = iscrizione.CodiceFiscale
 JOIN classevirtuale ON iscrizione.IdClasse = classevirtuale.IdClasse
 JOIN docente ON classevirtuale.CodiceFiscaleDocente = docente.CodiceFiscale
+LEFT JOIN classe_videogioco ON classevirtuale.IdClasse = classe_videogioco.IdClasse
+LEFT JOIN partita ON (classe_videogioco.IdVideogioco = partita.IdVideogioco AND studente.CodiceFiscale = partita.CodiceFiscale)
 WHERE studente.CodiceFiscale = ?
+GROUP BY classevirtuale.IdClasse, classevirtuale.Classe, classevirtuale.Materia, docente.Nome, docente.Cognome
 ");
 
 $stmt->bind_param("s", $codiceFiscale);
@@ -67,6 +75,7 @@ echo "<table border='1'>";
 echo    "<tr>";
 echo        "<th>Materia - Classe</th>";
 echo        "<th>Nome Docente</th>";
+echo        "<th>Monete</th>";
 echo        "<th>Apri</th>";
 echo    "</tr>";
 
@@ -74,12 +83,9 @@ while ($row = $result->fetch_assoc()) {
     echo "<tr>";
     echo    "<td>". htmlspecialchars($row['Materia'])." - ". htmlspecialchars($row['Classe']). "</td>";
     echo    "<td>". htmlspecialchars($row['Nome']) . " " . htmlspecialchars($row['Cognome']). "</td>";
+    echo    "<td>". htmlspecialchars($row['MoneteTotali']) . "</td>";
     echo    "<td>". "<a href='../routes/c.php?classe=" . $row['IdClasse'] . "'>Apri</a>" . "</td>";
     echo "</tr>";
 }
 
-
-
-
 echo "</table>";
-?>
